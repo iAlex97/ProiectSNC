55aa93147cf2edf34af16d395fbf4cb07ee784ea
Pangi <andrpangr@gmail.com>
2019-11-02 18:06:12 +0200

    added doc, and data for phase 1

---
+++ gen_spab.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	uint32_t reg = 0b11111'1111;
+	int nr_val = 48;
+	for (int i = 0; i < nr_val; i++) {
+		std::cout << (reg & 1) << ", ";
+
+		uint8_t last_bit = !!((1 << 0) & reg) != !!((1 << 4) & reg);
+		reg >>= 1;
+		reg = reg | (last_bit << 8);
+	}
+	std::cout << std::endl;
+	return 0;
+}
