39c11d7e31887dc788d80842ec74b39ae7abbbf1
Ionescu Alex <alx.ionescu97@gmail.com>
2019-12-15 14:59:13 +0200

    add some more

--- plot_data.m
+++
@@ -1,7 +0,0 @@
-res = open("b76_bun_RST.mat");
-
-hold on;
-plot(res.IOdata.time, res.IOdata.signals(1).values)
-plot(res.IOdata.time, res.IOdata.signals(2).values)
-plot(res.IOdata.time, res.IOdata.signals(3).values)
-hold off;
